249f31c27967fabb8b61debe777105b4569c63ab
Bas Nijholt <bas@nijho.lt>
2022-11-16 15:00:06 -0800

    session 5

--- exercises/hashmaps/hashmaps1.rs
+++ exercises/hashmaps/hashmaps1.rs
@@ -10,17 +10,19 @@
 //
 // Execute `rustlings hint hashmaps1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    let mut basket: HashMap<String, u32> = HashMap::new();
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
-
+    
     // TODO: Put more fruits in your basket here.
+    basket.insert(String::from("mango"), 2);
+    basket.insert(String::from("apple"), 1);
 
     basket
 }
@@ -38,6 +40,7 @@ mod tests {
     #[test]
     fn at_least_five_fruits() {
         let basket = fruit_basket();
+        // println!("{}", basket.values().sum::<u32>());
         assert!(basket.values().sum::<u32>() >= 5);
     }
 }
--- exercises/strings/strings2.rs
+++ exercises/strings/strings2.rs
@@ -2,16 +2,22 @@
 // Make me compile without changing the function signature!
 // Execute `rustlings hint strings2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 fn main() {
     let word = String::from("green"); // Try not changing this line :)
-    if is_a_color_word(word) {
+    if is_a_color_word(word.as_str()) {
         println!("That is a color word I know!");
     } else {
         println!("That is not a color word I know.");
     }
 }
+
+// q: what is difference between String and str?
+// a: String is a heap allocated string, str is a string slice
+
+// q: when to use str vs String?
+// a: when you want to own the string, use String, when you want to borrow the string, use str
 
 fn is_a_color_word(attempt: &str) -> bool {
     attempt == "green" || attempt == "blue" || attempt == "red"
